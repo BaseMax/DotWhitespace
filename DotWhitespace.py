af7230ae69edbb9eff9bc209682bb0e05dee8d4a
Max Base <MaxBaseCode@Gmail.Com>
2020-07-23 00:33:53 +0430

    Improve and solve bugs and add feature to read from File

--- DotWhitespace.py
+++ DotWhitespace.py
@@ -1,9 +1,14 @@
 # Max Base
 # DotWhitespace Interpreter
 # https://github.com/BaseMax/DotWhitespace
+# Date: 2020-07-17, 2020-07-23
+import sys
 import string
-chars=string.ascii_lowercase+string.ascii_uppercase+'0123456789!@#$%^&*()_-=+?<>[]{}'
+
+debug=False
+chars=' '+string.ascii_lowercase+string.ascii_uppercase+'0123456789!@#$%^&*()_-=+?<>[]{}'
 ops='+-*/^%'
+variable={}
 '''
 T      +
 TT     -
@@ -24,43 +29,32 @@ TTTTTT %
 ..ST  SS.S T SSS.S      ; PRINT 10 + 20
 ...
 '''
-input=''
-input+='. . .  .   .\t. .  .   .\n'
-input+='. . .  .   .\t\t. .  .   .\n'
-input+='. . .  .   .\t  .   .    . .\n'
-input+='.. \t. .  .   .\n'
-input+='.. . .  .   .\n'
-input+='..   . .\n'
-input+='..   . . .\t   . .\n'
-input+='..   . . \t   . \n'
 
-commands=input.split('\n')
-commands=list(filter(None, commands))
-variable={}
-## print(input)
-## print(commands)
 def calculate_number(values):
     result=0
     i=0
-    for value in values:
-        if value[1]==None:
+    # for value in values:
+    while i<len(values):
+        if values[i][1]==None:
             break
         else:
             values[i][0]=float(values[i][0])
             values[i+1][0]=float(values[i+1][0])
-            if value[1]=='+':
-                result+=values[i][0] + values[i+1][0]
-            elif value[1]=='-':
-                result+=values[i][0] - values[i+1][0]
-            elif value[1]=='*':
-                result+=values[i][0] * values[i+1][0]
-            elif value[1]=='/':
-                result+=values[i][0] / values[i+1][0]
-            elif value[1]=='^':
-                result+=values[i][0] ** values[i+1][0]
-            elif value[1]=='%':
-                result+=values[i][0] % values[i+1][0]
-            i+=1
+            if result==0:
+                result=values[i][0]
+            if values[i][1]=='+':
+                result=result + values[i+1][0]
+            elif values[i][1]=='-':
+                result=result - values[i+1][0]
+            elif values[i][1]=='*':
+                result=result * values[i+1][0]
+            elif values[i][1]=='/':
+                result=result / values[i+1][0]
+            elif values[i][1]=='^':
+                result=result ** values[i+1][0]
+            elif values[i][1]=='%':
+                result=result % values[i+1][0]
+        i+=1
     if i==0 and values[0][1]==None:
         result=float(values[0][0])
     if result==int(result):
@@ -68,78 +62,79 @@ def calculate_number(values):
     return result
 
 def parse_number(command):
+    global debug
     ## print('parsing number...')
     length=len(command)
     j=0
     value=''
     values=[]
-    for i in range(length):
+    # for i in range(length):
+    i=0
+    while i<length:
         if command[i]=='.':
-            ## print('\tINT', j-1)
             value+=str(j-1)
             j=0
-        elif command[i]=='\t':
+        elif command[i]=='\t' or command[i]=='T':
             op=0
             if command[i-1]!='.':
                 value+=str(j-1)
             for j in range(i+1, length):
-                if command[j]=='\t':
+                if command[j]=='\t' or command[j]=='T':
                     op+=1
                     i+=1
                 else:
                     break
-                ## print('j is:', j)
-
             values.append([value, ops[op]])
             value=''
             j=0
         else:
             j+=1
+        i+=1
     if j!=0:
-        ## print('\tINT', j-1)
         value+=str(j-1)
         j=0
     if value!='':
         values.append([value, None])
-    ## print(value)
-    ## print(values)
+    if debug:
+        print(values)
     return values
-    # return value
 
 def parse_string(command):
-    ## print('parsing string...')
     length=len(command)
     j=0
     value=''
     for i in range(length):
         if command[i]=='.':
-            ## print('\tCHAR', j+1, chars[j])
             value+=chars[j]
             j=0
-        elif command[i]=='\t':
+        elif command[i]=='\t' or command[j]=='T':
             return (value, i)
         else:
             j+=1
-    ## print(value)
-    # return value
     return (value, i)
 
 def parse_value(command):
-    if command[0]=='\t':
-        return('VAR', parse_string(command[1:])[0])
+    # print(command)
+    if command[0]=='\t' or command[0]=='T':
+        return('VAR', parse_string(command[1:])[0][1:])
     elif command[0]=='.':
-        return('STR', parse_string(command)[0])
+        return('STR', parse_string(command[1:])[0])
     else:
         return('NUM', calculate_number(parse_number(command)))
 
 def parse_command(command):
-    if command.startswith('... '):
-        print('READ') # TODO
-    elif command.startswith('.. '):
-        ## print('print')
-        ## print(command[3:])
+    global debug, variable
+    if command.lower() == '# debug' or command.lower() == '#debug':
+        debug=True
+    elif command.startswith('#'):
+        return
+    elif command.startswith('... ') or command.startswith('...S'):
+        if debug:
+            print('READ') # TODO
+    elif command.startswith('.. ') or command.startswith('..S'):
         command=command[3:]
-        # print('PRINT', parse_value(command))
+        if debug:
+            print('PRINT', parse_value(command))
         value=parse_value(command)
         if value[0] == 'VAR':
             try:
@@ -148,18 +143,31 @@ def parse_command(command):
                 print('not found')
         else:
             print(value[1])
-    elif command.startswith('. '):
-        ## print('define')
+    elif command.startswith('. ') or command.startswith('.S'):
         ident=parse_string(command[2:])
-        ## print(ident)
-        ## print(command)
-        ## print(command[ident[1]+3:])
         value=parse_value(command[ident[1]+3:])
-        # print('DEFINE', ident[0], value)
-        variable[ident[0]]=value
+        if debug:
+            print('DEFINE', ident[0][1:], value)
+        variable[ident[0][1:]]=value
 
-for command in commands:
-    ## print(command)
-    ## print(parse_command(command))
-    parse_command(command)
+def parse(input):
+    commands=input.split('\n')
+    commands=list(filter(None, commands))
+    for command in commands:
+        parse_command(command)
 
+def read(fileName):
+    return open(fileName).read()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        if sys.argv[1] == '-debug':
+            debug=True
+            parse( read( sys.argv[2] ) )
+        elif sys.argv[2] == '-debug':
+            debug=True
+            parse( read( sys.argv[1] ) )
+        else:
+            print("Error: bad arguments!")
+    if len(sys.argv) == 2:
+        parse( read( sys.argv[1] ) )
